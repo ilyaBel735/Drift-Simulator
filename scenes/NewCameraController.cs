e515904083d5893efa11c343fe45960bd31ca411
vghjhjk504-max <vghjhjk504@gmail.com>
2026-06-29 18:48:12 +0300

    Fix: Car camera

---
+++ scenes/NewCameraController.cs
@@ -0,0 +1,74 @@
+using Godot;
+
+public partial class NewCameraController : Camera3D
+{
+	[Export]
+	public float FollowDistance { get; set; } = 5.0f;
+
+	[Export]
+	public float FollowHeight { get; set; } = 2.0f;
+
+	[Export]
+	public float Speed { get; set; } = 20.0f;
+
+	[Export]
+	public Node3D FollowThis { get; set; }
+
+	private Vector3 _startRotation;
+	private Vector3 _startPosition;
+	private float pitchVar = 14f;
+
+	public override void _Ready()
+	{
+		_startRotation = Rotation;
+		_startPosition = Position;
+		FollowHeight = Position.Y;
+	}
+
+	public override void _PhysicsProcess(double delta)
+	{
+		if (FollowThis == null)
+			return;
+
+		Vector3 deltaV = GlobalTransform.Origin - FollowThis.GlobalTransform.Origin;
+		deltaV.Y = 0.0f;
+
+		if (deltaV.Length() > FollowDistance)
+		{
+			deltaV = deltaV.Normalized() * FollowDistance;
+			deltaV.Y = FollowHeight;
+			GlobalPosition = FollowThis.GlobalTransform.Origin + deltaV;
+		}
+
+		LookAt(FollowThis.GlobalTransform.Origin, Vector3.Up);
+
+		// После LookAt или вместо него
+		Vector3 targetPos = FollowThis.GlobalTransform.Origin;
+		Vector3 direction = (targetPos - GlobalPosition).Normalized();
+		// Желаемый угол наклона в радианах (например, -45° = -0.785 радиан)
+		float pitch = Mathf.DegToRad(pitchVar);
+		// Поворачиваем камеру так, чтобы она смотрела в направлении direction, но с дополнительным наклоном
+		Basis lookBasis = Basis.LookingAt(direction, Vector3.Up);
+		// Применяем поворот вокруг локальной оси X (питч)
+		lookBasis = lookBasis * new Basis(new Vector3(1, 0, 0), pitch);
+		GlobalTransform = new Transform3D(lookBasis, GlobalPosition);
+	}
+
+	// public override void _Input(InputEvent @event)
+	// {
+	// 	if (@event is InputEventKey input)
+	// 	{
+	// 		GD.Print(pitchVar);
+	// 		if (input.Keycode == Key.K)
+	// 		{
+	// 			pitchVar += -1;
+	// 		}
+	// 		if (input.Keycode == Key.L)
+	// 		{
+	// 			pitchVar += 1;
+	// 		}
+	// 	}
+
+	// }
+
+}
--- scripts/CameraController.cs
+++ scripts/CameraController.cs
@@ -5,6 +5,10 @@ public partial class CameraController : Node3D
     [Export] public float MouseSensitivity = 0.3f;
     [Export] public float RotationSpeed = 6.0f;
     [Export] private float touchSensitivity = 0.3f;
+    [Export] private float maxDownCamera = -40f;
+    [Export] private float maxUpCamera = 35f;
+    [Export] private Node3D targetCarPosition;
+    [Export] private Vector3 offset = Vector3.Zero;
 
     private bool rotating = false;
     private int rotationFingerIndex = -1;
@@ -16,6 +20,7 @@ public partial class CameraController : Node3D
     public override void _Ready()
     {
         IsMobile = OS.GetName() == "Android" || OS.GetName() == "iOS";
+        offset = Position;
     }
 
     public override void _Input(InputEvent @event)
@@ -47,8 +52,8 @@ public partial class CameraController : Node3D
                     targetRotation.X -= delta.X * touchSensitivity;
                     targetRotation.Y = Mathf.Clamp(
                         targetRotation.Y - delta.Y * touchSensitivity,
-                        -40,
-                        35
+                        maxDownCamera,
+                        maxUpCamera
                     );
                 }
             }
@@ -71,8 +76,8 @@ public partial class CameraController : Node3D
                 targetRotation.X -= mouseMotion.Relative.X * MouseSensitivity;
                 targetRotation.Y = Mathf.Clamp(
                     targetRotation.Y - mouseMotion.Relative.Y * MouseSensitivity,
-                    -40,
-                    35
+                    maxDownCamera,
+                    maxUpCamera
                 );
             }
         }
@@ -80,6 +85,8 @@ public partial class CameraController : Node3D
 
     public override void _Process(double delta)
     {
+        Position = targetCarPosition.Position + offset;
+
         currentRotation = currentRotation.Lerp(
             targetRotation,
             RotationSpeed * (float)delta
